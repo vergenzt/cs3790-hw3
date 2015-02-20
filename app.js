faef6c987476f4a362f99d204db754bcc086b661
tuuli <tuuli.pollanen@gmail.com>
2015-02-19 17:32:29 -0800

    Set up more dependencies, parsing data from the req body works wonderfully now. The last step is just to plug it in to MongoDB and write up the tutorial.

--- app.js
+++ app.js
@@ -2,12 +2,25 @@
 //they get saved in your node_modules directory, and you 'attach' them into your application
 //through require, in the following way:
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    body_parser = require('body-parser');
 //Instantiate our application with the Express framework:
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/public/views');
+//We are using schemaless Mongoose. Keep in mind that this is not a very good habit, but it 
+//works well for our minimalistic purposes where we just want to dump the data into the 
+//database and clean it up later. This enables it to work with different plugins that output
+//json objects with different parameters, but is a sub-optimal solution. The data you get out
+//from these experiments can easily be aggregated and fits well into a relational database format,
+//and I've successfully used jsPsych with Sequelize over a MySQL database in the past. This saves
+//you a HUGE head ache if you have a longer, more complicated test battery.
+
+var emptySchema = new mongoose.Schema({}, { strict: false });
+var Entry = mongoose.model('Entry', emptySchema);
+
+app.use(body_parser.json()); // to support JSON-encoded bodies
+app.use(body_parser.urlencoded({extended: true}));
+
 //This here is necessary because we want to serve just html pages.
 //Express works great with templating engines like ejs and jade, which
 //you can use to spice up your experiments by e.g. evaluating and passing
@@ -15,6 +28,9 @@ app.set('views', __dirname + '/public/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+//we need to tell express that we want to serve static files
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/views');
 //Here we tell the app where to find the static jsPsych files - it will resort to this
 //when it cannot find the static file it needs from the public folder
 app.use('/jsPsych', express.static(__dirname + "/jsPsych"));
@@ -37,7 +53,9 @@ app.get('/experiment', function(request, response) {
 })
 
 app.post('/experiment-data', function(request, response){
-    console.log('zomagad data post');
+    console.log(request.body);
+    // var entry = new Entry({ iAmNotInTheSchema: true });
+    // entry.save() // iAmNotInTheSchema is now saved to the db!!
     //Parse data from the AJAX call performed as on_finish-callback inside the jsPsych init call
     response.end();//and post that into your database
 })
