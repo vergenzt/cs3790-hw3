67a4675e7d7698b00fa6cb132e4d2e0bed8b451b
Tim Vergenz <vergenzt@gmail.com>
2015-06-01 09:44:24 -0400

    Style fixes

--- app.js
+++ app.js
@@ -1,55 +1,44 @@
-// --- LOADING MODULES
-var express = require('express'),
-    body_parser = require('body-parser'),
-    mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-// --- INSTANTIATE THE APP
 var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
-// --- MONGOOSE SETUP
+app.set('views', 'public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 mongoose.connect(process.env.CONNECTION || 'mongodb://localhost/jspsych'); 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
-    console.log('database opened');
+  console.log('database opened');
 });
 
 var emptySchema = new mongoose.Schema({}, { strict: false });
 var Entry = mongoose.model('Entry', emptySchema);
 
-// --- STATIC MIDDLEWARE 
-app.use(express.static(__dirname + '/public'));
-app.use('/jsPsych', express.static(__dirname + "/jsPsych"));
-
-// --- BODY PARSING MIDDLEWARE
-app.use(body_parser.json()); // to support JSON-encoded bodies
-
-// --- VIEW LOCATION, SET UP SERVING STATIC HTML
-app.set('views', __dirname + '/public/views');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-// --- ROUTING
+// routing
 app.get('/', function(request, response) {
-    response.render('index.html');
+  response.render('index.html');
 });
 
 app.get('/experiment', function(request, response) {
-    response.render('go_no_go.html');
+  response.render('go_no_go.html');
 });
 
 app.get('/finish', function(request, response) {
-    response.render('finish.html');
+  response.render('finish.html');
 })
 
-app.post('/experiment-data', function(request, response){
-    Entry.create({
-        "data":request.body
-    });    
-    response.end();
+app.post('/experiment-data', function(request, response) {
+  Entry.create({ data: request.body });  
+  response.end();
 })
 
-// --- START THE SERVER 
-var server = app.listen(process.env.PORT || 3000, function(){
-    console.log("Listening on port %d", server.address().port);
+var server = app.listen(process.env.PORT || 3000, function() {
+  console.log("Listening on port %d", server.address().port);
 });
+
