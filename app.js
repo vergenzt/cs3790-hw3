e9d0111fd96123558dd5594fd38f5148fe339c9b
Tim Vergenz <vergenzt@gmail.com>
2015-06-03 12:01:20 -0400

    Upload to S3 instead of mongolab

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var markdown = require('markdown-serve');
+var aws = require('aws-sdk');
+
+aws.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+var s3 = new aws.S3();
 
 var app = express();
 app.use(morgan('dev'));
@@ -20,10 +27,22 @@ app.get('/', function(req, res) {
   res.redirect('/experiment.html');
 });
 
-var db = require('./db');
-app.post('/experiment-data', function(request, response) {
-  db.Entry.create({ data: request.body });  
-  response.end();
+app.post('/experiment-data', function(req, res) {
+  //db.Entry.create({ data: request.body });
+  var params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: req.body.subject_id + ".csv",
+    Body: req.body.data_csv
+  };
+  s3.putObject(params, function(perr, pres) {
+    var text = params.Key + " to bucket " + params.Bucket
+    if (perr) {
+      console.log("Error uploading ", text, ": ", perr);
+    } else {
+      console.log("Successfully uploaded ", text);
+    }
+  });
+  res.end();
 })
 
 var server = app.listen(process.env.PORT || 3000, function() {
